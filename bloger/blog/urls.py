2d6783e5649a080db7802c46bfa512cea68f5a8b
vlasove <evgeny_vlasov@yahoo.com>
2020-03-04 12:57:50 +0300

    added pre CRUD

--- bloger/blog/urls.py
+++ bloger/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import BlogListView
+from .views import BlogListView, PostDetailView
 
 urlpatterns = [
-  path('', BlogListView.as_view(), name='home')
+  path('', BlogListView.as_view(), name='home'),
+  path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- bloger/blog/views.py
+++ bloger/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post 
 
@@ -9,3 +9,8 @@ class BlogListView(ListView):
   model = Post 
   template_name = 'home.html'
   context_object_name ='all_posts'
+
+class PostDetailView(DetailView):
+  model = Post 
+  template_name = 'post_detail.html'
+  context_object_name = 'post'
