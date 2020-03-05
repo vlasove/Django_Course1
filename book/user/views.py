3842d9fba5a5a47bbb1f52f0a3c783607cbe7cf1
vlasove <evgeny_vlasov@yahoo.com>
2020-03-05 11:49:07 +0300

    added book

---
+++ book/user/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class SignupView(CreateView):
+  form_class = CustomUserCreationForm
+  template_name = 'signup.html'
+  success_url = reverse_lazy('login')
